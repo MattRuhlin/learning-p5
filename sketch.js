ee2bec018d565e8584e6e8e9fa9275f0f1b8c225
Matt Ruhlin <mruhlin@plaid.com>
2018-05-28 20:39:16 -0700

    adjustable diameter circles on yellow

--- sketch.js
+++ sketch.js
@@ -1,34 +1,24 @@
-const DIAMETER = 20;
+const DIAMETER = 40;
 const CANVAS_WIDTH = 640;
 const CANVAS_HEIGHT = 480;
 
 function setup() {
-  console.log('here');
+  noLoop();
+
   createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT);
   background (255, 224, 137);
 }
 
 function draw() {
+  noLoop();
+
   console.log('here');
   let circlesAcross = CANVAS_WIDTH / (DIAMETER * 2);
   let circlesDown = CANVAS_HEIGHT / (DIAMETER * 2);
-  console.log(circlesAcross);
-  console.log(circlesDown);
-//  for (var i = 1; i <= circlesAcross; i++) {
-//    for (var j = 1; j <= circlesDown; i++) {
-//      ellipse(i * DIAMETER, j * DIAMETER, DIAMETER, DIAMETER);
-//    }
-//  }
-}
 
-/*
-
-function draw() {
-  if (mouseIsPressed) {
-    fill (0);
-  } else {
-    fill(255);
+  for (var i = .5; i <= circlesAcross; i++) {
+    for (var j = .5; j <= circlesDown; j++) {
+      ellipse(2 * i * DIAMETER, 2 * j * DIAMETER, DIAMETER, DIAMETER);
+    }
   }
-  ellipse(mouseX, mouseY, 80, 80);
 }
-*/
